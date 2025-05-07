d8f820f609f698caeee620ff3a6e8cb1d2810bcc
MaxFire120 <maxwellimo@gmail.com>
2025-05-07 13:12:59 -0600

    created the color.ps1 file

---
+++ color.ps1
@@ -0,0 +1,7 @@
+<#
+  Program Name : color.ps1
+  Date: 2025-05-07
+  Author: Maxwell Imo and Myciah Russell
+  Course: CYBER 360
+  We, Maxwell Imo and Myciah Russell, affirm that we wrote this script as original work completed by us.
+#>
